8c691db9ca2d752fe57271c1ad0e47bb7bf2a1ba
Kayswat <kazeemtoheeb@gmail.com>
2023-12-15 09:07:59 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const button = document.querySelector('button');
+const toastBox = document.querySelector('.toastBox');
+let successMsg = '<i class="fa-solid fa-circle-check"></i> Successfully submitted';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Please fix the error';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid input, please check again';
+
+function showToast(msg){
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast);
+
+    if(msg.includes('error')){
+        toast.classList.add('error');
+    }
+    if(msg.includes('Invalid')){
+        toast.classList.add('invalid');
+    }
+
+    setTimeout(()=>{
+        toast.remove();
+    },6000)
+}
+
+
+
+
+
+
